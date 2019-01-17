ae337aceaa65fa63dec5c340cdf2addd7b0090fd
wybei <734831833@qq.com>
2019-01-17 18:27:29 +0800

    提交代码

---
+++ 三子棋/三子棋/game.h
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define ROW 3
+#define COL 3
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+void InitBoard(char board[ROW][COL], int row, int col);
+void DisplayBoard(char board[ROW][COL], int row, int col);
+void playermove(char board[ROW][COL], int row, int col);
+void computermove(char board[ROW][COL], int row, int col);
+char iswin(char board[ROW][COL], int row, int col);
